e31de311b61dece5073f78a5ce102e97299cfbd7
xhuanhuan <805870540@qq.com>
2017-02-27 20:10:46 +0800

    complete webpack 配置

--- webpack/app/index.js
+++ webpack/app/index.js
@@ -1,11 +1,16 @@
-// import _ from 'lodash';
-
-function component () {
+import './part.html'
+import './style.css'
+var e1=require('./part.html');
+function component1 () {
   var element = document.createElement('div');
   let arr=[1,2,3];
-  element.innerHTML =  arr.map(item=>item+1).join(",")
-
+  element.innerHTML =  arr.map(item=>item*3).join(",")
   return element;
 }
-
-document.body.appendChild(component());
+function component2 () {
+  var element = document.createElement('div');
+  element.innerHTML = e1;
+  return element;
+}
+document.body.appendChild(component1());
+document.body.appendChild(component2());
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,29 @@
 var path = require('path');
-
+var webpack = require('webpack');
 module.exports = {
+  //入口
   entry: './app/index.js',
+  //出口
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+
+//服务器配置
+  devtool: "cheap-eval-source-map",
+  devServer:{
+    contentBase:path.resolve(__dirname,'./dev'),
+  },
+
+  //插件
+  plugins:[
+    new webpack.optimize.CommonsChunkPlugin({
+      name:'commons',
+      filename:'common.js',
+      minChunks:2,
+    }),
+  ],
+  //资源解析，打包
   module:{
     rules:[
       {
@@ -17,6 +35,34 @@ module.exports = {
         },
       }],
       exclude:/node_modules/
+    },
+    {
+       test: /\.css$/,
+       use: [ 'style-loader', 'css-loader' ]
+    },
+    {
+      test: /\.(sass|scss)$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader',
+      ]
+    },
+    {
+      test:/\.html$/,
+      use:['html-loader'],
+    },
+    {
+      test:/\.hbs$/,
+      use:['handlebars-loader'],
+    },
+
+    {
+       test: /\.(png|jpg)$/,
+       use: [
+         'url-loader',
+         'file-loader',
+       ]
     }
     ]
   }
